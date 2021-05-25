fce901871653eedd4dd9cf218f05cfc1f00e9f32
SawadaHaruka <65148869+SawadaHaruka@users.noreply.github.com>
2021-05-25 23:15:43 +0900

    5/25
    extended settimeout()

--- main.js
+++ main.js
@@ -278,7 +278,7 @@ const init = () => {
       createjs.Tween.get(winner).to({ alpha: 1 }, 1300, createjs.Ease.circOut);
     }
   }
-  setTimeout(timeup, 10000);
+  setTimeout(timeup, 12000);
 
 }
 init();
